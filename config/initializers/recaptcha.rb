cd0776e264977c24b5d57b1ff8039823d9612052
thomas jouvel <thomas.jouvel@gmail.com>
2020-12-03 08:50:30 +0100

    fixing recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,8 @@
 Recaptcha.configure do |config|
-  config.site_key  = Rails.application.credentials.RECAPTCHA_SITE_KEY
-  config.secret_key = Rails.application.credentials.RECAPTCHA_SECRET_KEY
+  # config.site_key  = Rails.application.credentials.recaptcha_site_key
+  config.site_key  = "6LfXgPYZAAAAAIumuzJd1KOx5ZdmS4SibPpD09F2"
+  # config.secret_key = Rails.application.credentials.recaptcha_secret_key
+  config.secret_key = "6LfXgPYZAAAAAC2E91Sd_CsFbODS8Xc94pTaNF1G"
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
 end
