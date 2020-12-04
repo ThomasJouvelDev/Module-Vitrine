0702898d018e7422a21c5034d2a58400adf788c8
thomas jouvel <thomas.jouvel@gmail.com>
2020-12-04 09:24:37 +0100

    change mailer from, recaptcha keys

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,13 +2,13 @@ class ContactMailer < ApplicationMailer
 
   def general_message(contact)
     @contact = contact
-    mail( :from => @contact.email, :to => "dev@thomasjouvel.com", :subject => "Vous avez un message de votre Site")
+    mail( :to => "dev@thomasjouvel.com", :subject => "Vous avez un message de votre Site")
   end
 
   def produit_message(contact, produit)
     @article = produit
     @contact = contact
-    mail( :from => @contact.email, :to => "dev@thomasjouvel.com", :subject => "Demande d'information sur #{produit.nom}")
+    mail( :to => "dev@thomasjouvel.com", :subject => "Demande d'information sur #{produit.nom}")
   end
 
 end
